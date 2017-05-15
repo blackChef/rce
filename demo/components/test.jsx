493e40617f6f9f3e803cc8470c124654520d4e95
blackChef <hongpidai@gmail.com>
2017-05-15 11:40:07 +0800

    add cursorProps feature

--- demo/components/test.jsx
+++ demo/components/test.jsx
@@ -3,19 +3,39 @@ import createComponent from '../../package/createComponent';
 import uncontrolled from '../../package/createModelHolder';
 import { view as Counter, init as counterInit } from './counter';
 
+let Inner = createComponent({
+  name: 'Inner',
+  view(props) {
+    // console.log('render inner');
+    console.log(props);
+    return null;
+  }
+});
+
 let name = 'test';
 
-let init = counterInit;
+let init = function() {
+  return [1,2,3,4,5];
+};
 
 let update = function({ type, payload, model, dispatch }) {};
 
 let view = React.createClass({
+  getInitialState() {
+    return { time: 0 };
+  },
+
+  componentDidMount() {
+    // setInterval(() => {
+    //   this.setState({ time: Date.now() });
+    // }, 1000);
+  },
+
   render() {
     let { model } = this.props;
-
     return (
       <div>
-        test
+        <Inner foo={model} cursorProps={['foo']}/>
       </div>
     );
   },
