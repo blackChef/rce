38f97d607c075172d5ccf278d29db1e797912cc8
blackChef <hongpidai@gmail.com>
2017-06-13 14:52:50 +0800

    add length, fix toArray

--- package/array/toArray.jsx
+++ package/array/toArray.jsx
@@ -1,7 +1,7 @@
 import isInteger from 'lodash/isInteger';
 
 let toArray = function(node) {
-  let keys = Object.keys(node).filter(isInteger);
+  let keys = Object.keys(node).filter(i => isInteger(+i));
   return keys.map(function(key) {
     return node[key];
   });
